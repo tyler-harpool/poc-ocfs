f590ef28c6c1a4becc259f540eb337515dc2909b
Tyler Harpool <tharpool@jackhenry.com>
2024-06-06 03:28:19 -0500

    Fix schema migrations

--- database_utils/migrations/20240606081759_status.up.sql
+++ database_utils/migrations/20240606081759_status.up.sql
@@ -8,18 +8,17 @@ CREATE TABLE IF NOT EXISTS status (
     juv TEXT,
     crim TEXT,
     traf TEXT,
-    dataElement TEXT,
+    data_element TEXT,
     definition TEXT,
     values JSONB,
-    currentlyCollected TEXT,
-    ifNoIsThisNeeded TEXT,
-    ifYesWhere TEXT,
+    currently_collected TEXT,
+    if_no_is_this_needed TEXT,
+    if_yes_where TEXT,
     comments TEXT
 );
 
-INSERT INTO status (
-    civ, fam, prob, dep, juv, crim, traf, dataElement, definition, values, currentlyCollected, ifNoIsThisNeeded, ifYesWhere, comments
-) VALUES
+INSERT INTO status (civ, fam, prob, dep, juv, crim, traf, data_element, definition, values, currently_collected, if_no_is_this_needed, if_yes_where, comments)
+ VALUES
 ('x', 'x', 'x', 'x', 'x', 'x', 'x', 'Case Status', 'The current status of the case', '["open", "closed", "pending", "resolved"]'::jsonb, NULL, NULL, NULL, NULL),
 ('x', 'x', 'x', 'x', 'x', 'x', 'x', 'Hearing Status', 'The status of the hearing related to the case', '["scheduled", "postponed", "cancelled", "held"]'::jsonb, NULL, NULL, NULL, NULL),
 ('x', 'x', 'x', 'x', 'x', 'x', 'x', 'Filing Status', 'The status of the filings in the case', '["filed", "not filed", "rejected", "accepted"]'::jsonb, NULL, NULL, NULL, NULL),

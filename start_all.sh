d69564d344996d433b36101c928ec156c69c2e08
Tyler Harpool <tharpool@jackhenry.com>
2024-06-06 05:09:27 -0500

    Added start_all script

---
+++ start_all.sh
@@ -0,0 +1,18 @@
+cargo run -p adr_api & sleep 10s;
+cargo run -p case_data_api & sleep 5s;
+cargo run -p sanctions_api & sleep 5s;
+cargo run -p gateway_api & sleep 5s;
+cargo run -p attorney_advocate_api & sleep 5s;
+cargo run -p charges_api & sleep 5s;
+cargo run -p civil_judgments_api & sleep 5s;
+cargo run -p dependency_permanency_api & sleep 5s;
+cargo run -p diversion_api & sleep 5s;
+cargo run -p hearings_events_api & sleep 5s;
+cargo run -p motions_filings_api & sleep 5s;
+cargo run -p orders_api & sleep 5s;
+cargo run -p participants_api & sleep 5s;
+cargo run -p pleadings_api & sleep 5s;
+cargo run -p post_trial_api & sleep 5s;
+cargo run -p pretrial_intake_api & sleep 5s;
+cargo run -p probate_api & sleep 5s;
+cargo run -p status_api
